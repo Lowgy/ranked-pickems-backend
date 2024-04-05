136e19e29419191a0f71b8ab7b5886485a726945
Logan Olson <loganolsonn@gmail.com>
2024-04-04 19:32:42 -0500

    Match resource generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PickModule } from './pick/pick.module';
 import { ParticipantModule } from './participant/participant.module';
+import { MatchModule } from './match/match.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PickModule, ParticipantModule],
+  imports: [UserModule, AuthModule, PickModule, ParticipantModule, MatchModule],
   controllers: [],
   providers: [],
 })
